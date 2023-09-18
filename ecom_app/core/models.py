a6d5c9a1c5a7e227891be1276d9f16897a4b79f0
Kanu2518 <abhishekvamja2518@gmail.com>
2023-09-18 16:44:26 +0530

    Update User Model.

--- ecom_app/core/models.py
+++ ecom_app/core/models.py
@@ -36,7 +36,7 @@ class UserManager(BaseUserManager):
         return user
     
 
-class User(AbstractBaseUser,BaseUserManager):
+class User(AbstractBaseUser,PermissionsMixin):
     """User in the system."""
     email = models.EmailField(max_length=255,unique=True)
     name = models.CharField(max_length=255)
